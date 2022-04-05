02a8f01e639d26b82cff0f83069c1d92fa5a3b47
prithve-rice <98857972+prithve-rice@users.noreply.github.com>
2022-04-05 13:50:59 -0500

    Update RunDexter.sh

--- e301/RunDexter.sh
+++ e301/RunDexter.sh
@@ -1,3 +1,7 @@
+#Making sure that the LED connected to GPIO58 can be turned on
+#Activating the usr3 LED to show that the program is active
+#Running the Dexter voice assistant
+
 echo "out" > /sys/class/gpio/gpio58/direction
 echo 1 > /sys/class/leds/beaglebone:green:usr3/brightness
 python3 RunDexter.py
