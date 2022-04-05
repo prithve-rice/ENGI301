977ed534c6c1c61b61febe46ecc1ca4f0cde3d4d
prithve-rice <98857972+prithve-rice@users.noreply.github.com>
2022-04-05 13:52:17 -0500

    Update RecordAudio.sh

--- e301/RecordAudio.sh
+++ e301/RecordAudio.sh
@@ -1,3 +1,5 @@
+#A script to record audio, activating the LED connected to GPIO58 to indicate when recording is active.
+
 echo 1 > /sys/class/gpio/gpio58/value
 arecord -D hw:0,0 -f s16_le testing.wav -d 3 -r44100
 echo 0 > /sys/class/gpio/gpio58/value
