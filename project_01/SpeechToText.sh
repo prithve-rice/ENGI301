531a274dae1a6e53e7dd59d9f13afc8c6e8d1ca2
prithve-rice <98857972+prithve-rice@users.noreply.github.com>
2022-05-07 02:52:10 -0500

    reorg

---
+++ project_01/SpeechToText.sh
@@ -0,0 +1,2 @@
+#Running the brain of dexter, called when the always listening system hears the wakeword.
+python3 SpeechToText.py
---
+++ project_01/WakeWord.sh
@@ -0,0 +1,5 @@
+#Running a hacked version of the Porcupine demo, modified to run according to our purposes. Make sure the entire Porcupine github library is located in /var/lib/cloud9
+#and the modified porcupine_demo_mic is in the appropriate location.
+
+./../Porcupine/demo/c/build/porcupine_demo_mic -l /var/lib/cloud9/Porcupine/lib/beaglebone/libpv_porcupine.so -m /var/lib/cloud9/Porcupine/lib/common/porcupine_params.pv -k /var/lib/cloud9/e301/dexter_bb.ppn -t 0.5 -a bRbEu5EJcrUrtsY529EroZuTnFWBqDTXLnG83u4C/UgTpOefxHgMXw== -d 2
+
